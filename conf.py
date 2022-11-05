be17abe2db363226526f283d211f16bafb62cd45
Nix Verfeles <hiro.developers@gmail.com>
2022-11-05 20:15:42 +0900

    Add files via upload

--- conf.py
+++ conf.py
@@ -24,10 +24,15 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'alabaster'
 html_theme = 'furo'
+myst_number_code_blocks = ["python"]
 
 html_static_path = ['_static']
 
-#numfig = True
-myst_number_code_blocks = ["python"]
+github_css = r"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/github.min.css"
+
+diff2html_css = r"https://cdn.jsdelivr.net/npm/diff2html/bundles/css/diff2html.min.css"
+diff2html_js = r"https://cdn.jsdelivr.net/npm/diff2html/bundles/js/diff2html-ui-slim.min.js"
+
+html_css_files = [github_css, diff2html_css]
+html_js_files = [diff2html_js, 'd2hset.js']
